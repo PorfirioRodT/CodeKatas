cea71ac0025251cba76e4ae114e6a5e645b4b3e2
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-05-18 21:55:19 -0400

    creacion del proyecto y avances en los test y creacion de la clase del programa principal

---
+++ CheckAnagrams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace TestAnagrams
+{
+    public class CheckAnagrams
+    {
+
+        public IEnumerable<string> GetFilePath(string path)
+        {
+
+            return File.ReadAllLines(path).GroupBy(x => string.Concat(x.OrderBy(i => i))).Where(x => x.Count() > 1)
+                       .Select(x => {return x.Key + " " + x.FirstOrDefault();});
+
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TestAnagrams
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AreAnagramas()
+        {
+
+            CheckAnagrams anagrams = new CheckAnagrams();
+
+            int setsOfAnagrams = 20683;
+
+            IEnumerable<string> anagrams = anagrams.Get() 
+
+            Assert.That(() => anagrams.AreAnagrams("Juan", "nuja"), Is.True);
+
+        }
+
+        [Test]
+        public void AreNotAnagramas()
+        {
+
+            CheckAnagrams anagrams = new CheckAnagrams();
+
+            Assert.That(() => anagrams.AreAnagrams("Juan", "pedro"), Is.False /* Throws.Nothing*/);
+
+        }
+
+        [Test]
+        public void TheListOfWordsIsEmpy()
+        {
+
+            CheckAnagrams anagrams = new CheckAnagrams();
+
+            Assert.That(() => anagrams.AreAnagrams(null), Throws.ArgumentNullException);
+
+        }
+
+    }
+}
